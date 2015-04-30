79dc2ca3de0d4f0b73ec2baf5476df59d405c575
Harvey Chapman <hchapman@3gfp.com>
2015-04-30 10:55:06 -0400

    Play a single video in a VideoView on start up

--- app/src/main/java/com/rdm/rdmtestplayer/PlayerActivity.java
+++ app/src/main/java/com/rdm/rdmtestplayer/PlayerActivity.java
@@ -2,8 +2,12 @@ package com.rdm.rdmtestplayer;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.VideoView;
 
 
 public class PlayerActivity extends Activity {
@@ -12,6 +16,16 @@ public class PlayerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        ViewGroup.LayoutParams params
+                = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+        VideoView videoView = new VideoView(this);
+        FrameLayout rootFrame = (FrameLayout)findViewById(R.id.rootFrame);
+        rootFrame.addView(videoView, params);
+
+        videoView.setVideoURI(Uri.parse("/sdcard/media/BeerDemo.mp4"));
+        videoView.start();
     }
 
     @Override
